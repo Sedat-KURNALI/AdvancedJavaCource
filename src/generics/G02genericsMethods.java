7cc450b35e9504ff06cab108e19d594df3c02a5f
Sedat-KURNALI <sedatkurnali@gmail.com>
2022-09-19 18:07:28 +0300

    2022.19.09_v02

--- src/generics/G02genericsMethods.java
+++ src/generics/G02genericsMethods.java
@@ -19,25 +19,43 @@ public class G02genericsMethods {
         Character[] charDizi = {'r', 'e', 'c', 'e', 'p'};
         //task 1 generic method create ederek bu dizileri yazdirirniz
         diziYazdir(intDizi);
-        System.out.println("\n***********");
         diziYazdir(doubleDizi);
-        System.out.println("\n***********");
         diziYazdir(charDizi);
-        System.out.println("\n***********");
+
         //task 2 max sayiyi bulan generic method yazinir. 3 pmli olsun
-
-
         System.out.println(" - generic method ");
+        System.out.println("maxBulGeneric(35,63,34) = " + maxBulGeneric(35, 63, 34));
+        System.out.println("maxBulGeneric(\"21\",\"35\",\"55\") = " + maxBulGeneric("21", "35", "55"));
+        System.out.println("maxBulGeneric(35.2, 63.2, 34.3) = " + maxBulGeneric(35.2, 63.2, 34.3));
+
+
     }
 
-    public static <T> void diziYazdir(T[] dizi) {
+    public static <T> void diziYazdir(T[] dizi) {//generic method create edildi
         Arrays.stream(dizi).forEach(System.out::print);
     }
 
-    public static <T extends Comparable<T>> T maxBulGeneric(T a, T b, T c) {
+    public static <T extends Comparable <T> > T maxBulGeneric(T a, T b, T c) {
+        //bunu generic return type olan bir methoda cvevirin
+        //cevap => comparable interface i kullanilmali
         T max = a;
-        //b.Comp(max)<0
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
         return max;
     }
 
+
 }
+
+
+
+
+
+
+
+
+
