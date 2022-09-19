8768d7bec2137451528a48b0e1ae94259fa472a5
Sedat-KURNALI <sedatkurnali@gmail.com>
2022-09-19 17:55:03 +0300

    2022.19.09_v02

--- src/generics/G02genericsMethods.java
+++ src/generics/G02genericsMethods.java
@@ -16,26 +16,28 @@ public class G02genericsMethods {
     public static void main(String[] args) {
         Integer[] intDizi = {1, 2, 3, 3, 6, 4, 6, 5, 6, 4, 6, 9};
         Double[] doubleDizi = {1.2, 2.6, 3.2, 3.8, 6.3};
-        Character[] charDizi = {'a', 's', 'w', 'r'};
-    //task 1 generic method create ederek bu dizileri yazdirirniz
-
-     //task 2 max sayiyi bulan generic method yazinir. 3 pmli olsun
-
+        Character[] charDizi = {'r', 'e', 'c', 'e', 'p'};
+        //task 1 generic method create ederek bu dizileri yazdirirniz
+        diziYazdir(intDizi);
+        System.out.println("\n***********");
+        diziYazdir(doubleDizi);
+        System.out.println("\n***********");
+        diziYazdir(charDizi);
+        System.out.println("\n***********");
+        //task 2 max sayiyi bulan generic method yazinir. 3 pmli olsun
 
 
         System.out.println(" - generic method ");
     }
 
+    public static <T> void diziYazdir(T[] dizi) {
+        Arrays.stream(dizi).forEach(System.out::print);
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public static <T extends Comparable<T>> T maxBulGeneric(T a, T b, T c) {
+        T max = a;
+        //b.Comp(max)<0
+        return max;
+    }
 
 }
